90cb2443f2f50307ec7897323d2618216acb1f07
Michael Wallio <rawrizaur@gmail.com>
2012-02-03 13:58:15 -0500

    Added a check to make sure that the percent of memory used isn't over 100, and added it to reset to default color before the script exists.

--- memUsage.sh
+++ memUsage.sh
@@ -4,6 +4,11 @@
 memUsage=`top -l 1 | awk '/PhysMem/ {printf "%d", $8}'`
 memUsage=`expr ${memUsage} \* 100 / 2048`
 
+if [ ${memUsage} -gt 100 ]
+then
+    memUsage=100
+fi
+
 typeset -i m=5
 
 while [ ${m} -lt ${memUsage} ]
@@ -19,6 +24,7 @@ do
     echo -ne "\n——"
     m=`expr ${m} + 5`
 done
+echo -ne "\033[0m"
 echo ""
 unset memUsage
 unset m
